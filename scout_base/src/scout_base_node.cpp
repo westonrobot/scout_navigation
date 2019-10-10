ef72005125eaa66c2958fc459d8af239a8d615b4
Ruixiang Du <ruixiang.du@westonrobot.com>
2019-10-10 11:13:11 +0800

    updated launch files and readme

--- scout_base/src/scout_base_node.cpp
+++ scout_base/src/scout_base_node.cpp
@@ -16,18 +16,28 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "scout_odom");
     ros::NodeHandle node(""), private_node("~");
 
-    // instantiate a robot
+    // instantiate a robot object
     ScoutBase robot;
     ScoutROSMessenger messenger(&robot, &node);
 
-    std::string scout_can_port;
-    private_node.param<std::string>("port_name", scout_can_port, std::string("can0"));
+    // fetch parameters before connecting to robot
+    std::string port_name;
+    private_node.param<std::string>("port_name", port_name, std::string("can0"));
     private_node.param<std::string>("odom_frame", messenger.odom_frame_, std::string("odom"));
     private_node.param<std::string>("base_frame", messenger.base_frame_, std::string("base_link"));
     private_node.param<bool>("simulated_robot", messenger.simulated_robot_, false);
 
-    // connect to scout and setup ROS subscription
-    robot.Connect(scout_can_port);
+    // connect to robot and setup ROS subscription
+    if (port_name.find("can") != std::string::npos)
+    {
+        robot.Connect(port_name);
+        ROS_INFO("Using CAN bus to talk with the robot");
+    }
+    else
+    {
+        robot.Connect(port_name, 115200);
+        ROS_INFO("Using UART to talk with the robot");
+    }
     messenger.SetupSubscription();
 
     // publish robot state at 20Hz while listening to twist commands
